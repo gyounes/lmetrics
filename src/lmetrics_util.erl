e9c3aef627d852af750a4f1fc326f749dfb51cfe
Georges Younes <georges.r.younes@gmail.com>
2017-05-30 14:04:13 +0100

    add size in lmetric util

--- src/lmetrics_util.erl
+++ src/lmetrics_util.erl
@@ -5,7 +5,8 @@
 %% ldb_util callbacks
 -export([atom_to_binary/1,
          binary_to_atom/1,
-         unix_timestamp/0]).
+         unix_timestamp/0,
+         size/2]).
 
 %% @doc
 -spec atom_to_binary(atom()) -> binary().
@@ -22,3 +23,20 @@ binary_to_atom(Binary) ->
 unix_timestamp() ->
     {Mega, Sec, _Micro} = erlang:timestamp(),
     Mega * 1000000 + Sec.
+
+%% @doc
+-spec size(term | crdt | delta_buffer | pure_crdt, term()) -> non_neg_integer().
+size(term, T) ->
+    erts_debug:flat_size(T);
+size(crdt, CRDT) ->
+    state_type:crdt_size(CRDT);
+size(delta_buffer, DeltaBuffer) ->
+    lists:foldl(
+        fun({Sequence, {From, CRDT}}, Acc) ->
+            Acc + size(crdt, CRDT) + size(term, {Sequence, From})
+        end,
+        0,
+        DeltaBuffer
+    );
+size(pure_crdt, CRDT) ->
+    pure_type:crdt_size(CRDT).
